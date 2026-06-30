2ca1eb1d8a55d50bce4a692931a32844dc8b81c2
saurav296 <sauravsumanjnvm2937@gmail.com>
2026-06-30 22:37:13 +0530

    ui: setup global styles, app icons, and main root layout

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Outfit } from "next/font/google";
+import { AuthProvider } from "@/contexts/AuthContext";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const outfit = Outfit({
+  variable: "--font-outfit",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Fundr- Crowdfunding dApp",
+  description:
+    "A modern crowdfunding dApp for secure and transparnet funding for your next campaighns",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} ${outfit.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
+  );
+}
